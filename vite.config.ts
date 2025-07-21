54652c0a4f28286abee1e96b577dfc8bf61dc204
Pedro Hernandez <pedroinformatico@gmail.com>
2025-07-21 12:48:25 -0400

    Actualiza la configuración del proyecto: se modifica la URL del repositorio y la página de inicio en package.json. Se ajusta la estructura de salida de la librería en vite.config.ts, incluyendo la configuración para la construcción de la misma y la gestión de dependencias. Se añaden tipos y archivos necesarios para la distribución.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,22 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/odontograma/index.ts'),
+      name: 'OpOdontogram',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    copyPublicDir: false
+  }
 });
